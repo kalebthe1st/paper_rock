07f89cf9b3ed0a74a8ecdc36660a37df1e9c1eef
kalebthe1st <88403660+kalebthe1st@users.noreply.github.com>
2023-09-21 15:14:50 -0700

    added the javascript edits and styles to ze button

--- javascript.js
+++ javascript.js
@@ -1,70 +1,111 @@
+const start = document.getElementById("start");
+
+start.addEventListener("click", game());
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
+
+        return "Draw " + playerSelection + " equals " + computerSelection
+    }
+    else {
+        if (playerSelection === "rock" && computerSelection === "scissor") {
+            pla++;
+            return "You win! " + playerSelection + " beats " + computerSelection;
+        } else if (playerSelection === "scissor" && computerSelection === "paper") {
+            pla++;
+            return "You win! " + playerSelection + " beats " + computerSelection;
+        } else if (playerSelection === "paper" && computerSelection === "rock") {
+            pla++;
+            return "You win! " + playerSelection + " beats " + computerSelection;
+        } else {
+            com++;
+            return "You lose! " + computerSelection + " beats " + playerSelection;
+        }
+    }
+}
+
+function getComputerChoice() {
+    number = 3
+
+    function random(number) {
+        return Math.floor(Math.random() * number);
+    }
+    choice = random(number);
+    switch (choice) {
+        case 0:
+            return "paper"
+            break;
+
+        case 1:
+            return "scissor"
+            break;
+
+        // include as many cases as you like
+
+        default:
+            return "rock"
+            break;
+    }
+}
+function score() {
+    if (com > pla) {
+        console.log("The winner is computer by a score of " + com + " - " + pla);
+
+    }
+    else {
+        console.log("You win by a score of " + pla + " - " + com);
+
+    }
+}
+
 function game() {
     com = 0;
     pla = 0;
 
-    function getComputerChoice() {
-        number = 3
+    const heading = document.querySelector('#player');
+    heading.textContent = `${pla} - ${com}`;
 
-        function random(number) {
-            return Math.floor(Math.random() * number);
-        }
-        choice = random(number);
-        switch (choice) {
-            case 0:
-                return "paper"
-                break;
 
-            case 1:
-                return "scissor"
-                break;
 
-            // include as many cases as you like
+    const btn = document.querySelectorAll('button');
 
-            default:
-                return "rock"
-                break;
-        }
-    }
+    let choiceplayer = "";
 
-    for (let i = 0; i < 5; i++) {
+    // we use the .forEach method to iterate through each button
+    btn.forEach((button) => {
 
-        computerSelection = getComputerChoice();
-        let choiceplayer = prompt('Enter rock, paper or scissor', '');
-        playerSelection = choiceplayer.toLowerCase();
+        // and for each one we add a 'click' listener
+        button.addEventListener('click', () => {
+            if (button.id !== "start") {
 
-        function playRound(playerSelection, computerSelection) {
+                choiceplayer = (button.id);
+                alert(choiceplayer);
+                playerSelection = choiceplayer.toLowerCase();
+                computerSelection = getComputerChoice();
 
-            if (playerSelection === computerSelection) {
 
-                return "Draw " + playerSelection + " equals " + computerSelection
-            }
-            else {
-                if (playerSelection === "rock" && computerSelection === "scissor") {
-                    pla++;
-                    return "You win! " + playerSelection + " beats " + computerSelection;
-                } else if (playerSelection === "scissor" && computerSelection === "paper") {
-                    pla++;
-                    return "You win! " + playerSelection + " beats " + computerSelection;
-                } else if (playerSelection === "paper" && computerSelection === "rock") {
-                    pla++;
-                    return "You win! " + playerSelection + " beats " + computerSelection;
-                } else {
-                    com++;
-                    return "You lose! " + computerSelection + " beats " + playerSelection;
+                console.log(playRound(playerSelection, computerSelection))
+                heading.textContent = `${pla} - ${com}`;
+
+
+                if (com === 5 || pla === 5) {
+                    score();
+                    com = 0;
+                    pla = 0;
                 }
+
             }
-        }
-        console.log(playRound(playerSelection, computerSelection))
-    }
-    if (com > pla) {
-        console.log("The winner is com by a score of " + com);
+            else if (button.id === 'start') {
+                com = 0;
+                pla = 0;
+            }
+        });
 
-    }
-    else if (pla > com) {
-        console.log("You win by a score of " + pla);
 
-    }
-    else {
-        console.log("It's a draw");
-    }
+    });
+
+
 }
+
+
+// }
